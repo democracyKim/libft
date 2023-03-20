bc7cb476c779dbe4908204e04f6b986c1fef8cc3
Minju Kim <minkim3@c4r6s8.42seoul.kr>
2023-03-20 15:36:49 +0900

    strtok

--- string/ft_strtok.c
+++ string/ft_strtok.c
@@ -6,24 +6,24 @@
 /*   By: minkim3 <minkim3@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/20 15:23:35 by minkim3           #+#    #+#             */
-/*   Updated: 2023/03/20 15:26:48 by minkim3          ###   ########.fr       */
+/*   Updated: 2023/03/20 15:32:55 by minkim3          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../libft.h"
 
-char	*ft_strtok(char **str, const char *delim)
+char	*ft_strtok(char **str, const char *sep)
 {
 	char	*start;
 	char	*ptr;
 
-	if (!str || !*str || !delim)
+	if (!str || !*str || !sep)
 		return (NULL);
 	start = *str;
 	ptr = *str;
 	while (*ptr)
 	{
-		if (ft_strchr(delim, *ptr))
+		if (ft_strchr(sep, *ptr))
 		{
 			*ptr = '\0';
 			*str = ptr + 1;
